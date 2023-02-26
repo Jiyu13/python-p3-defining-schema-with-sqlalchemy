41822713832abbb3f16c4b7723470489357efc89
Ziru Zou <jiyucoding@yahoo.com>
2023-02-26 18:51:12 -0500

    code along

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -3,10 +3,29 @@
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
+from sqlalchemy import create_engine
+
 Base = declarative_base()
 
+
+# Inheritance from a declarative_base object
+# data model
 class Student(Base):
-    pass
+    
+    # class attribute, used as the name of SQL db table
+    __tablename__ = "students"
+
+    # class attributes
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+
 
 if __name__ == '__main__':
-    pass
+    engine = create_engine("sqlite:///students.db")
+    Base.metadata.create_all(engine)
+
+
+
+# run chmod +x lib/sqlalchemy_sandbox.py to maje the script executable
+# run lib/sqlalchemy_sandbox.py python shell to create db with students table
+
